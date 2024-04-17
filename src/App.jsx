e01d423eade328163f1805c0fb9e3c2009e44d48
TheoAt <theo.atlani@gmail.com>
2024-04-17 13:12:21 +0200

    initialisation of the project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='main-container'>
+      <h1 className="main-title">Quote Generator in ReactJS</h1>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/QuoteGenerator.jsx
@@ -0,0 +1,7 @@
+export default function QuoteGenerator() {
+    return(
+        <div className="quote-container">
+            
+        </div>
+    )
+}
