9a06b98efa8d53c760eff42a256ca2f1db2fcebd
TheoAt <theo.atlani@gmail.com>
2024-04-18 15:09:15 +0200

    add quote section, with fetch calling and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import QuoteGenerator from './components/QuoteGenerator';
 
 function App() {
   return (
     <div className='main-container'>
       <h1 className="main-title">Quote Generator in ReactJS</h1>
+      <QuoteGenerator />
     </div>
   )
 }
--- src/components/QuoteGenerator.jsx
+++ src/components/QuoteGenerator.jsx
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react"
+import './quote.css'
+
 export default function QuoteGenerator() {
-    return(
+
+    const [isLoading, setIsLoading] = useState(false)
+    const [quote, setQuote] = useState()
+
+    async function fetchQuote() {
+        try {
+
+            setIsLoading(true)
+            const apiResponse = await fetch('https://api.quotable.io/quotes/random', {
+                method: 'GET'
+            })
+            const result = await apiResponse.json()
+
+            if (result && result.length > 0) {
+                setIsLoading(false)
+                setQuote(result[0])
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchQuote()
+    }, [])
+
+    const handleChangeQuote = () => {
+        fetchQuote()
+    }
+
+    return (
         <div className="quote-container">
-            
+            {
+                isLoading ?
+                    <p className="loading">Loading quote ! Please wait a few secondes...</p>
+                    :
+                    <div className="quote-content">
+                        <p className="quote">{quote?.content}</p>
+                        <p className="quote-author">{quote?.author}</p>
+                    </div>
+            }
+
+            <button className="quote-button" onClick={handleChangeQuote}>New Quote</button>
         </div>
     )
 }
